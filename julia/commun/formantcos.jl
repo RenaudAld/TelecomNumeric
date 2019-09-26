9fec8778fce66db7661d9ffe0421dcff8d833384
Mokopower <marin.merlin@me.com>
2019-09-26 10:54:10 +0200

    Corrected error in formantcos.jl at line 4

--- julia/commun/formantcos.jl
+++ julia/commun/formantcos.jl
@@ -1,7 +1,7 @@
 function formantcos(TAILLE, SURECHANTILLONNAGE)
     PI = 3.1415926535;
     formant = zeros(TAILLE);
-    t = (collect(1:1:TAILLE) - floor((TAILLE+1)/2)) / SURECHANTILLONNAGE;
+    t = (collect(1:1:TAILLE) .- floor((TAILLE+1)/2)) / SURECHANTILLONNAGE;
     for i=1:TAILLE
         if (t[i]*t[i]) == (1/16)
             formant[i] = PI / 4;
