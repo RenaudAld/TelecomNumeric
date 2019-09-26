4daae6d07fb610e06a8d10abe63864935dc132f4
Allemand Renaud <allemand_ren@mfopt14.metz.supelec.fr>
2019-09-26 10:38:17 +0200

    Add formantcos

---
+++ julia/commun/formantcos.jl
@@ -0,0 +1,13 @@
+function formantcos(TAILLE, SURECHANTILLONNAGE)
+    PI = 3.1415926535;
+    formant = zeros(TAILLE);
+    t = (collect(1:1:TAILLE) - floor((TAILLE+1)/2)) / SURECHANTILLONNAGE;
+    for i=1:TAILLE
+        if (t[i]*t[i]) == (1/16)
+            formant[i] = PI / 4;
+        else
+            formant[i] = cos(2*PI*t[i]) ./ (1 - 16 * t[i] * t[i]);
+        end;
+    end
+return formant;
+end
