9fbf308a3f5fd71d719f1b5a04a01d6a8779481d
Mokopower <marin.merlin@me.com>
2019-09-26 10:34:19 +0200

    Added code in formantrect

---
+++ julia/commun/formantrect.jl
@@ -0,0 +1,13 @@
+function formantrect(TAILLE, SURECHANTILLONNAGE)
+    PI = 3.1415926535;
+    formant = zeros(TAILLE);
+    t = (collect(1:1:TAILLE) .- Int(floor((TAILLE+1)/2))) / SURECHANTILLONNAGE;
+    for i=1:TAILLE
+        if t[i] == 0
+            formant[i] = 1;
+        else
+            formant[i] = sin(PI*t[i]) ./ (PI*t[i]);
+        end
+    end
+    return formant;
+end
