3a0c96b63204d01749a533e08c38987a5d42b73a
Mokopower <marin.merlin@me.com>
2019-10-24 12:12:20 +0200

    Added new error method which takes in account canal convolution (synchronisation is good but havent changed binary energy yet)

--- julia/taux_erreurs_canal/erreur_canal.jl
+++ julia/taux_erreurs_canal/erreur_canal.jl
@@ -1,11 +1,14 @@
+using DSP;
 include("../commun/bruit.jl")
 include("../commun/emission.jl")
 include("../commun/reception.jl")
-function erreur(Pb,TAILLE,SURECHANTILLONNAGE,formant,filtre)
+function erreur_canal(Pb,TAILLE,SURECHANTILLONNAGE,formant,filtre,canal)
     TAILLE_FORMANT = 100;
+    TAILLE_CANAL = length(canal);
     message = 2 .* Int.(rand(TAILLE) .> 0.5) .- 1;
     signal = emission(message, formant, SURECHANTILLONNAGE);
     signal = signal + bruit(Pb, (formant'*formant)[1], size(signal,1));
-    recu = reception(signal, filtre, SURECHANTILLONNAGE, 1+TAILLE_FORMANT*SURECHANTILLONNAGE);
+    signal = conv(signal, canal);
+    recu = reception(signal, filtre, SURECHANTILLONNAGE, (TAILLE_CANAL-1)/2+TAILLE_FORMANT*SURECHANTILLONNAGE);
     return(sum(abs.(recu-message)/2)/TAILLE);
 end
