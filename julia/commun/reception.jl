8d1cf7e7ce17ceee85411a09a7b3ba5288a931bc
Mokopower <marin.merlin@me.com>
2019-10-24 12:13:58 +0200

    Changed synchronisation factor in the receptor

--- julia/commun/reception.jl
+++ julia/commun/reception.jl
@@ -1,8 +1,10 @@
 using DSP;
 function reception(signal,filtre,SURECHANTILLONNAGE,decalage)
     int_decalage = Int(decalage)
+    print(int_decalage);
+    print("\n");
     signal_conv = conv(signal,filtre);
-    signal_tronc = signal_conv[int_decalage*2-1:1:length(signal_conv)-int_decalage*2+1+SURECHANTILLONNAGE] #A voir le troncage, c'est fait a la main je comprend pas pourquoi faut rajouter 1+SURECHANTILLONNAGE
+    signal_tronc = signal_conv[int_decalage:1:length(signal_conv)-int_decalage+1]
     signal_echant = signal_tronc[1:SURECHANTILLONNAGE:length(signal_tronc)]
     signal_seuil = (signal_echant .> 0) .* 2 .- 1 
     return(signal_seuil);
--- julia/commun/testrecepteur.jl
+++ julia/commun/testrecepteur.jl
@@ -12,8 +12,7 @@ formant = formantcos(SURECHANTILLONNAGE*TAILLE_FORMANT+1, SURECHANTILLONNAGE);
 signal = emission(message, formant, SURECHANTILLONNAGE);
 filtre = formant[end:-1:1] / (formant'*formant);
 filtre = filtre[1:end,1];
-recu,ana = reception(signal, filtre, SURECHANTILLONNAGE, 1+TAILLE_FORMANT*SURECHANTILLONNAGE/2);
-plot(axe_temporel(length(ana), SURECHANTILLONNAGE, SURECHANTILLONNAGE*TAILLE_FORMANT),ana)
+recu = reception(signal, filtre, SURECHANTILLONNAGE, 1+TAILLE_FORMANT*SURECHANTILLONNAGE);
+#plot(axe_temporel(length(ana), SURECHANTILLONNAGE, SURECHANTILLONNAGE*TAILLE_FORMANT),ana)
 plot(recu)
-#plot(message)
 sum(abs.(recu-message)/2)
