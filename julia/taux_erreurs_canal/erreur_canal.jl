4e0c071f771d1ee8dbd3d0d8c44560cb5b779bc6
Allemand Renaud <allemand_ren@mfopt14.metz.supelec.fr>
2019-10-31 09:39:25 +0100

    Before merge

--- julia/taux_erreurs_canal/erreur_canal.jl
+++ julia/taux_erreurs_canal/erreur_canal.jl
@@ -2,13 +2,14 @@ using DSP;
 include("../commun/bruit.jl")
 include("../commun/emission.jl")
 include("../commun/reception.jl")
-function erreur_canal(Pb,TAILLE,SURECHANTILLONNAGE,formant,filtre,canal)
-    TAILLE_FORMANT = 100;
+function erreur_canal(Pb,TAILLE,SURECHANTILLONNAGE,TAILLE_FORMANT,formant,filtre,canal)
     TAILLE_CANAL = length(canal);
     message = 2 .* Int.(rand(TAILLE) .> 0.5) .- 1;
     signal = emission(message, formant, SURECHANTILLONNAGE);
-    signal = signal + bruit(Pb, (formant'*formant)[1], size(signal,1));
     signal = conv(signal, canal);
-    recu = reception(signal, filtre, SURECHANTILLONNAGE, (TAILLE_CANAL-1)/2+TAILLE_FORMANT*SURECHANTILLONNAGE);
+    signal = signal + bruit(Pb, (formant'*formant)[1], size(signal,1));
+    recu = reception(signal, filtre, SURECHANTILLONNAGE, (TAILLE_CANAL - 1) + (TAILLE_FORMANT) * SURECHANTILLONNAGE);
+    print(length(recu))
+    print("\n")
     return(sum(abs.(recu-message)/2)/TAILLE);
 end
